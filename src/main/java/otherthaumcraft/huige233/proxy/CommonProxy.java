67cefac8398e81ef9a8134da68ee54fd778c9960
huige <61108540+huige233@users.noreply.github.com>
2022-05-12 22:42:36 +0800

    Add files via upload

---
+++ src/main/java/otherthaumcraft/huige233/proxy/CommonProxy.java
@@ -0,0 +1,21 @@
+package otherthaumcraft.huige233.proxy;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy {
+    public void registerItemRenderer( Item item, int meta, String id )
+    {
+    }
+
+
+    public void preInit( FMLPreInitializationEvent event )
+    {
+    }
+
+
+    public void init( FMLInitializationEvent event )
+    {
+    }
+}
---
+++ src/main/java/otherthaumcraft/huige233/util/Reference.java
@@ -0,0 +1,10 @@
+package otherthaumcraft.huige233.util;
+
+public class Reference {
+    public static final String MOD_ID = "otherthaumcraft";
+    public static final String NAME = "OtherThaumcraft";
+    public static final String VERSION = "1.0.0";
+    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+    public static final String CLIENT_PROXY_CLASS = "otherthaumcraft.huige233.proxy.ClientProxy";
+    public static final String COMMON_PROXY_CLASS = "otherthaumcraft.huige233.proxy.CommonProxy";
+}
