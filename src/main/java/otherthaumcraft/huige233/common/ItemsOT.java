c4542349ad4c0cdb5e362bbe79f18fcdfc27c078
KELETU66666 <92129388+KELETU66666@users.noreply.github.com>
2022-05-14 07:30:16 +0800

    Create ItemsOT.java

---
+++ src/main/java/otherthaumcraft/huige233/common/ItemsOT.java
@@ -0,0 +1,16 @@
+package com.verdantartifice.thaumicwonders.common.items;
+
+import com.verdantartifice.thaumicwonders.ThaumicWonders;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemBow;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+@GameRegistry.ObjectHolder(ThaumicWonders.MODID)
+public class ItemsTW {
+    public static final Item DISJUNCTION_CLOTH = null;
+    public static final ItemSword PRIMAL_DESTROYER = null;
+    public static final Item FLYING_CARPET = null;
+}
