5a2044023a100f93e82965d3f21629f0b665e9b4
a <EYCHFSJD@gmail.com>
2022-05-15 21:04:32 +0800

    更改部分文件

--- src/main/java/huige233/otherthaumcraft/init/InitRecipes.java
+++ src/main/java/huige233/otherthaumcraft/init/InitRecipes.java
@@ -39,10 +39,10 @@ public class InitRecipes {
 private static void initArcaneRecipes() {
                 ThaumcraftApi.addArcaneCraftingRecipe(new ResourceLocation(Reference.MOD_ID, "warp_paper"), new ShapedArcaneRecipe(
                 defaultGroup,
-                "",
+                "WARP_PAPER",
                 30,
                 new AspectList(),
-                new ItemStack(ItemsOT.WARP_PAPER),
+                new ItemStack(ItemsOT.WARP_PAPER,3,0)
                     "PSP",
                     "   ",
                     "   ",
@@ -51,22 +51,52 @@ private static void initArcaneRecipes() {
 }
         
 private static void initCrucibleRecipes() {
-        ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(Reference.MOD_ID, "taint_meat"), new CrucibleRecipe(
-                "",
+        ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(Reference.MOD_ID, "taint_meat1"), new CrucibleRecipe(
+                "TAINT_MEAT",
                 new ItemStack(ItemsOT.TAINT_MEAT),
                 new ItemStack(Items.CHICKEN),
                 new AspectList().add(Aspect.FLUX, 20).add(Aspect.EXCHANGE, 10)
-        ));}
+        ));
+    ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(Reference.MOD_ID, "taint_meat2"), new CrucibleRecipe(
+            "TAINT_MEAT",
+            new ItemStack(ItemsOT.TAINT_MEAT),
+            new ItemStack(Items.PORKCHOP),
+            new AspectList().add(Aspect.FLUX, 20).add(Aspect.EXCHANGE, 10)
+    ));
+    ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(Reference.MOD_ID, "taint_meat3"), new CrucibleRecipe(
+            "TAINT_MEAT",
+            new ItemStack(ItemsOT.TAINT_MEAT),
+            new ItemStack(Items.BEEF),
+            new AspectList().add(Aspect.FLUX, 20).add(Aspect.EXCHANGE, 10)
+    ));
+    ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(Reference.MOD_ID, "taint_meat4"), new CrucibleRecipe(
+            "TAINT_MEAT",
+            new ItemStack(ItemsOT.TAINT_MEAT),
+            new ItemStack(Items.MUTTON),
+            new AspectList().add(Aspect.FLUX, 20).add(Aspect.EXCHANGE, 10)
+    ));
+    ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(Reference.MOD_ID, "taint_meat5"), new CrucibleRecipe(
+            "TAINT_MEAT",
+            new ItemStack(ItemsOT.TAINT_MEAT),
+            new ItemStack(Items.RABBIT),
+            new AspectList().add(Aspect.FLUX, 20).add(Aspect.EXCHANGE, 10)
+    ));
+    ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(Reference.MOD_ID, "taint_meat6"), new CrucibleRecipe(
+            "TAINT_MEAT",
+            new ItemStack(ItemsOT.TAINT_MEAT),
+            new ItemStack(Items.FISH),
+            new AspectList().add(Aspect.FLUX, 20).add(Aspect.EXCHANGE, 10)
+    ));}
 private static void initInfusionRecipes() {
       ThaumcraftApi.addInfusionCraftingRecipe(new ResourceLocation(Reference.MOD_ID, "cleansing_amulet"), new InfusionRecipe(
-                "",
+                "CLEANSING_AMULET",
                 new ItemStack(ItemsOT.CLEANSING_AMULET),
                 8,
                 new AspectList().add(Aspect.MIND, 100).add(Aspect.ORDER, 75).add(Aspect.AURA, 50).add(Aspect.ENERGY, 50).add(Aspect.EXCHANGE, 30),
                 new ItemStack(ItemsTC.amuletVis),
                 new Object[] {
-                        new ItemStack(ItemsTC.sanitySoap),
                         new ItemStack(ItemsTC.bathSalts),
+                        new ItemStack(ItemsTC.sanitySoap),
                         new ItemStack(ItemsTC.bathSalts),
                         Ingredient.fromItem(ItemsTC.primordialPearl),
                 }
