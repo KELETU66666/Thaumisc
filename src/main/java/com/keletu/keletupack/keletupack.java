f90ee0076f529d5065a61e7fbb8e09b849cbaa67
KELETU66666 <92129388+KELETU66666@users.noreply.github.com>
2022-05-23 21:55:04 +0800

    Add files via upload

---
+++ src/main/java/com/keletu/keletupack/keletupack.java
@@ -0,0 +1,13 @@
+package com.keletu.keletupack;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = keletupack.MODID, name = keletupack.NAME, version =keletupack.VERSION, acceptedMinecraftVersions =keletupack.acceptedMinecraftVersions)
+public class keletupack {
+public static final String
+
+    MODID = "keletupack";
+    public static final String NAME = "Keletu's Pack";
+    public static final String VERSION = "1.0";
+    public static final String acceptedMinecraftVersions = "1.12.2";
+}
