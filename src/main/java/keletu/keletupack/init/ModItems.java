a610e7b8f1c075e4df9494266befceff3c6187a7
a <EYCHFSJD@gmail.com>
2022-06-12 16:49:50 +0800

    马鞭

--- src/main/java/keletu/keletupack/init/ModItems.java
+++ src/main/java/keletu/keletupack/init/ModItems.java
@@ -26,6 +26,7 @@ public class ModItems {
     public static final ItemFood NrtherSwart  = new food("nether_swart", 2, 0.4F, false, keletupack.ITEM_TAB);
     public static final Item.ToolMaterial MATERIAL_ICHORIUM = EnumHelper.addToolMaterial("ichorium", 6, -1, 8.0F, 4.0F, 22);
     public static final Item.ToolMaterial MATERIAL_MORPH = EnumHelper.addToolMaterial("morph", 7, 1500, 8.0F, 3.0F, 15);
+    public static final Item.ToolMaterial MATERIAL_LEATHER_K = EnumHelper.addToolMaterial("ridingcrop", 0, 64, 0.0F, 0.0F, 10);
     public static final Item.ToolMaterial MATERIAL_ICHORIUM_AWAKENED = EnumHelper.addToolMaterial("ichorium_awakened", 7, -1, 10.0F, 6.0F, 22);
     public static final ItemArmor.ArmorMaterial MATERIAL_ICHOR = EnumHelper.addArmorMaterial("ichor", Reference.MOD_ID + ":ichor", -1, new int[]{3, 6, 8, 3}, 22, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 2.0F);
     public static final ItemSword IchoriumSword = new IchoriumSword("ichorium_sword", keletupack.ITEM_TAB, MATERIAL_ICHORIUM);
@@ -48,4 +49,5 @@ public class ModItems {
     public static final ItemPickaxe MorphPickaxe = new MorphPick("morph_pick", keletupack.ITEM_TAB, MATERIAL_MORPH);
     public static final ItemAxe MorphAxe = new MorphAxe("morph_axe", keletupack.ITEM_TAB, MATERIAL_MORPH);
     public static final ItemPickaxe DISTORTIONPICK = new DistortionPick("distortion_pick", keletupack.ITEM_TAB, MATERIAL_MORPH);
+    public static final ItemSword RIDINGCROP = new RidingCrop("riding_crop", keletupack.ITEM_TAB, MATERIAL_LEATHER_K);
 }
---
+++ src/main/java/keletu/keletupack/items/tools/RidingCrop.java
@@ -0,0 +1,92 @@
+package keletu.keletupack.items.tools;
+
+import keletu.keletupack.init.ModItems;
+import keletu.keletupack.keletupack;
+import keletu.keletupack.util.IHasModel;
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.EntityGolem;
+import net.minecraft.entity.passive.EntityHorse;
+import net.minecraft.entity.passive.EntityPig;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+
+public class RidingCrop extends ItemSword implements IHasModel {
+
+    public RidingCrop(String name, CreativeTabs tab, ToolMaterial material) {
+        super(material);
+        setTranslationKey(name);
+        setRegistryName(name);
+        setCreativeTab(tab);
+
+        ModItems.ITEMS.add(this);
+    }
+
+    @Override
+    public boolean hitEntity(ItemStack stack, EntityLivingBase victim, EntityLivingBase player) {
+        stack.damageItem(1, player);
+        if (victim instanceof EntityHorse || victim instanceof EntityPig)
+            victim.addPotionEffect(new PotionEffect(MobEffects.SPEED, 200, 5, false, false));
+        else if (victim instanceof EntityPlayer || victim instanceof EntityGolem) {
+            victim.addPotionEffect(new PotionEffect(MobEffects.SPEED, 100, 1, false, false));
+            victim.addPotionEffect(new PotionEffect(MobEffects.HASTE, 200, 1, false, false));
+            victim.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 100, 1, false, false));
+        }
+        if (!player.world.isRemote && player.world.provider.getDimension() == -1 && player.world.rand.nextInt(15) == 1) {
+            EntityItem ent = victim.entityDropItem(new ItemStack(Items.REDSTONE, 1, 0), 1.0F);
+            ent.motionY += player.world.rand.nextFloat() * 0.05F;
+            ent.motionX += (player.world.rand.nextFloat() - player.world.rand.nextFloat()) * 0.1F;
+            ent.motionZ += (player.world.rand.nextFloat() - player.world.rand.nextFloat()) * 0.1F;
+        }
+        return true;
+    }
+
+
+    @Override
+    public EnumAction getItemUseAction(ItemStack stack) {
+        return EnumAction.NONE;
+    }
+
+    @Override
+    public int getMaxItemUseDuration(ItemStack stack) {
+        return 0;
+    }
+
+    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+        ItemStack itemstack = player.getHeldItem(hand);
+        if (player.getRidingEntity() != null && player.getRidingEntity() instanceof EntityLivingBase) {
+            EntityLivingBase mount = (EntityLivingBase) player.getRidingEntity();
+            itemstack.damageItem(1, player);
+            player.getSwingProgress(1);
+            mount.attackEntityFrom(DamageSource.GENERIC, 1.0F);
+            mount.addPotionEffect(new PotionEffect(MobEffects.SPEED, 200, 5, false, false));
+        }
+        return new ActionResult<>(EnumActionResult.PASS, itemstack);
+    }
+
+    @Override
+    public boolean getIsRepairable(ItemStack stack, ItemStack stack2) {
+        return stack2.getItem() == Items.LEATHER || super.getIsRepairable(stack, stack2);
+    }
+
+    @Override
+    public void registerModels() {
+        keletupack.proxy.registerItemRenderer(this, 0, "inventory");
+    }
+}
