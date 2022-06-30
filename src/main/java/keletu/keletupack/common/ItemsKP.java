3bd75acbdce259cb4101e95abca73a67f9bf48b8
a <EYCHFSJD@gmail.com>
2022-06-30 21:52:38 +0800

    修复愚蠢的错误

--- src/main/java/keletu/keletupack/common/ItemsKP.java
+++ src/main/java/keletu/keletupack/common/ItemsKP.java
@@ -4,7 +4,7 @@ import keletu.keletupack.items.EnderShard;
 import keletu.keletupack.items.NetherShard;
 import keletu.keletupack.items.warppaper;
 import keletu.keletupack.util.Reference;
-keletu.keletupack.items.baubles.*;
+import keletu.keletupack.items.baubles.*;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
