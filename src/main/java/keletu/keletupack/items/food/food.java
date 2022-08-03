cb8a2f9906aa9416077d92e23c32e747d07a3e97
a <EYCHFSJD@gmail.com>
2022-08-03 18:02:25 +0800

    地狱蛋糕优化

--- src/main/java/keletu/keletupack/items/food/food.java
+++ src/main/java/keletu/keletupack/items/food/food.java
@@ -1,23 +1,20 @@
-package keletu.keletupack.items;
+package keletu.keletupack.items.food;
 
 import keletu.keletupack.init.ModItems;
 import keletu.keletupack.keletupack;
 import keletu.keletupack.util.IHasModel;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemFood;
-import net.minecraft.potion.PotionEffect;
 
-public class NetherCake extends ItemFood implements IHasModel {
-    public NetherCake(String name, int amount, float saturation, boolean isWolfFood, CreativeTabs tab) {
+public class food extends ItemFood implements IHasModel {
+    public food(String name, int amount, float saturation, boolean isWolfFood, CreativeTabs tab) {
         super(amount, saturation, isWolfFood);
         setUnlocalizedName(name);
         setRegistryName(name);
         setCreativeTab(tab);
-        setPotionEffect(new PotionEffect(MobEffects.SATURATION, 1, 5), 1F);
+        setAlwaysEdible();
         ModItems.ITEMS.add(this);
     }
-
     @Override
     public void registerModels() {
         keletupack.proxy.registerItemRenderer(this, 0, "inventory");
